2b0f6bbd03d46bd9f4db1ff6aaedbc6477520897
JG1220 <118772029+JG1220@users.noreply.github.com>
2023-02-20 14:52:34 -0500

    Create security group

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default" {
   default = true
 }
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -24,10 +25,9 @@ resource "aws_instance" "blog" {
   vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Learning Terraform"
   }
 }
-
 
 resource "aws_security_group" "blog" {
   name        = "blog"
@@ -46,13 +46,12 @@ resource "aws_security_group" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-
 resource "aws_security_group" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidr_vlocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -62,7 +61,7 @@ resource "aws_security_group" "blog_everything_out" {
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cidr_vlocks = ["0.0.0.0/0"]
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
