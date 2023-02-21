f963420702d96cbb51ec29d37075ca37bbbb068a
JG1220 <118772029+JG1220@users.noreply.github.com>
2023-02-21 11:54:57 -0500

    test

--- main.tf
+++ main.tf
@@ -15,7 +15,8 @@ data "aws_ami" "app_ami" {
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
 
   name = "dev"
   cidr = "10.0.0.0/16"
@@ -32,9 +33,9 @@ module "vpc" {
 }
 
 resource "aws_instance" "blog" {
+  
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -45,13 +46,13 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
-  name = "blog"
+  name    = "blog"
 
-  vpc_id = module.vpc.public_subnets[1]
+  vpc_id = module.vpc.public_subnets[0]
 
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
