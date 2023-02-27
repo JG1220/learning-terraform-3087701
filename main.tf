f7588461174f01020c7aee822ab6b4a03f3fc85d
JG1220 <118772029+JG1220@users.noreply.github.com>
2023-02-27 12:49:46 -0500

    Create VPC

--- main.tf
+++ main.tf
@@ -34,8 +34,8 @@ module "vpc" {
 
 resource "aws_instance" "blog" {
   
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -45,10 +45,11 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
-  name    = "blog"
-
-  vpc_id = module.vpc.public_subnets[0]
+  
+  version     = "4.13.0"
+  name        = "blog"
+  description = "Creating security group module"
+  vpc_id      = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
