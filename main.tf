a3002c995f4f427e7870e87f2e43534807949333
JG1220 <118772029+JG1220@users.noreply.github.com>
2023-02-28 11:39:16 -0500

    lb5

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module "alb" {
 
   vpc_id = module.vpc.vpc_id
   subnets = module.vpc.public_subnets
-  security_groups = list(module.blog_sg.security_group_id)
+  security_groups = tolist[(module.blog_sg.security_group_id)]
 
   target_groups = [
     {
