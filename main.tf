30f37442a40d4f3cafdb36d75117ddcb6b529123
JG1220 <118772029+JG1220@users.noreply.github.com>
2023-02-28 11:27:25 -0500

    lb4

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id = vpc-0e1bb2770ea0b9ede
+  vpc_id = module.vpc.vpc_id
   subnets = module.vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
